fc6c09c17295f4027e178e793589168bb3c4b1cf
emrepbu <33737009+emrepbu@users.noreply.github.com>
2025-05-04 18:23:03 +0300

    feat: Add dependencies and plugins for Firebase, Hilt, Room, and DataStore
    This commit introduces several new dependencies and plugins to the project:
    
    -   **Firebase:** Added dependencies for Firebase Authentication, Firestore, and Storage. Configured the `google-services` plugin.
    -   **Hilt:** Added dependencies for Hilt for Android, Hilt Work, and Hilt Navigation Compose. Enabled the `com.google.dagger.hilt.android` plugin and `ksp` plugin.
    -   **Room:** Added dependencies for Room runtime and compiler (`ksp`).
    -   **DataStore:** Added dependency for DataStore Preferences.
    -   **Gson:** Added dependency for Gson library.
    -  **Navigation compose**: Added dependency for navigation compose.
    - **Work manager**: Added dependency for WorkManager.
    - **KSP**: Added KSP plugin for Kotlin symbol processing.
    - Updated dependency version: Updated Compose BOM version to 2025.04.01, and added new version values for datastorePreferences, hiltAndroid, hiltWork, and ksp.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
+    alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -43,12 +46,15 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +62,22 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.work)
+    ksp(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+
+    implementation(libs.gson)
+    implementation(libs.androidx.datastore.preferences)
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx")
 }
